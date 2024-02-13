c40d82a33e7d6e94e6264a76dc5acd23cb641646
Tanakorn45645 <153884448+Tanakorn45645@users.noreply.github.com>
2024-02-13 17:23:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tipc65410140/MainActivity.kt
@@ -0,0 +1,146 @@
+package com.example.tipc65410140
+
+import android.os.Bundle
+import android.widget.NumberPicker.OnValueChangeListener
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+//import androidx.compose.foundation.gestures.ModifierLocalScrollableContainerProvider.value
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tipc65410140.ui.theme.TipC65410140Theme
+import java.text.NumberFormat
+
+import androidx.compose.material3.Icon
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+var amountInput: MutableState<String> = mutableStateOf("0")
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipC65410140Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TipTimeLayout()
+                }
+            }
+        }
+    }
+
+    fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+        val tip = tipPercent / 100 * amount
+        return NumberFormat.getCurrencyInstance().format(tip)
+    }
+    @Composable
+    fun EditNumberField(
+        value: String,
+        onValueChange: (String) -> Unit,
+        label: Int,
+        modifier: Modifier = Modifier
+    ) {
+        TextField(
+            value = value,
+            onValueChange = onValueChange,
+
+            //value = amountInput,
+            //onValueChange = { amountInput = it },
+            singleLine = true,
+            label = { Text(stringResource(R.string.bill_amount)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = modifier
+        )
+    }
+
+    @Composable
+    fun TipTimeLayout() {
+        var amountInput by remember { mutableStateOf("") }
+        var tipInput by remember { mutableStateOf("") }
+        val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+        val amount = amountInput.toDoubleOrNull() ?: 0.0
+
+        val tip = calculateTip(amount, tipPercent)
+
+        Column(
+            modifier = Modifier.padding(40.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.calculate_tip),
+                modifier = Modifier
+                    .padding(bottom = 16.dp, top = 40.dp)
+                    .align(alignment = Alignment.Start)
+            )
+            EditNumberField(
+                value = amountInput,
+                onValueChange = { amountInput = it },
+                label = R.string.bill_amount,
+                modifier = Modifier
+                    .padding(bottom = 32.dp)
+                    .fillMaxWidth()
+            )
+            EditNumberField(
+                label = R.string.how_was_the_service,
+                value = tipInput,
+                onValueChange = { tipInput = it },
+                modifier = Modifier
+                    .padding(bottom = 32.dp)
+                    .fillMaxWidth()
+            )
+
+            Text(
+                text = stringResource(R.string.tip_amount, tip),
+                style = MaterialTheme.typography.displaySmall
+            )
+            Spacer(modifier = Modifier.height(150.dp))
+
+        }
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        TipC65410140Theme {
+            TipTimeLayout()
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "tipC65410140"
+include(":app")
+ 
